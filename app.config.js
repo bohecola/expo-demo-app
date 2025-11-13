5261ba5bf6011115005b4cad203d4ade516b7186
bohecola <bohecola@outlook.com>
2025-11-13 16:44:03 +0800

    init

---
+++ app.config.js
@@ -0,0 +1,46 @@
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    return 'com.yourname.stickersmash.dev';
+  }
+
+  if (IS_PREVIEW) {
+    return 'com.yourname.stickersmash.preview';
+  }
+
+  return 'com.yourname.stickersmash';
+};
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return 'StickerSmash (Dev)';
+  }
+
+  if (IS_PREVIEW) {
+    return 'StickerSmash (Preview)';
+  }
+
+  return 'StickerSmash: Emoji Stickers';
+};
+
+
+export default ({ config }) => ({
+  ...config,
+  name: getAppName(),
+  ios: {
+    ...config.ios,
+    bundleIdentifier: getUniqueIdentifier(),
+  },
+  android: {
+    ...config.android,
+    package: getUniqueIdentifier(),
+  },
+  updates: {
+    url: 'https://u.expo.dev/dfccecbb-0704-4457-877d-b382b8ae6c8e',
+  },
+  runtimeVersion: {
+    policy: 'appVersion',
+  },
+});
